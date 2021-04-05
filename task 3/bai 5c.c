f4383a768d1ba364caed6847cf1a56f62d9a491a
vietnguyen <nguyenvanvietbn1998@gmail.com>
2021-04-05 10:29:49 +0700

    Create bai 5c.c

---
+++ task 3/bai 5c.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	int main(int argc, char *argv[]) {
+	
+	float inh,feet,a;
+	printf("Nhap so thuc a(cm): "); scanf("%f",&a);
+	inch=a/2.54;
+	feet=a/(2.54*12);
+	printf("So inch tuong ung la:%.1f \n",inh);
+	printf("So feet tuong ung la:%.1f ",feet);
+
+	return 0;
+}
